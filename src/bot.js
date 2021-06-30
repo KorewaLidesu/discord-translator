0d1781e930f65df3e369f3037bc4a8213c5ad173
Li <nhatlinh.l195@gmail.com>
2021-06-30 18:44:30 +0700

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -6,7 +6,6 @@ require("dotenv").config();
 const discord = require("discord.js");
 const client = new discord.Client();
 const auth = require("./core/auth");
-const request = require('request');
 
 //
 // Event Listener
@@ -15,36 +14,6 @@ const request = require('request');
 const events = require("./events");
 
 events.listen(client);
-
-client.on('message', messagelogger => {
-  if (auth.readch.includes(messagelogger.channel.id)) {
-    let content = messagelogger.content;
-    messagelogger.attachments.forEach(attachment => {
-      content += '\n' + attachment.proxyURL;
-    });
-
-    auth.writech.forEach(channel => {
-      client.channels.get(channel).send(content, {embed: messagelogger.embeds[0]}).catch(err => {
-        console.error(err);
-      });
-    });
-
-    auth.webhook.forEach(webhook => {
-      request({
-        url: webhook,
-        method: 'POST',
-        json: {
-          content: content,
-          embeds: messagelogger.embeds,
-        },
-      }, err => {
-        if (err) {
-          console.error(err);
-        }
-      });
-    });
-  }
-});
 
 //
 // Initialize Bot
